83ff16c150db291788c57f8685ec209bb5cd7c26
victor Odonnell <39573414+vipereaper@users.noreply.github.com>
2024-05-26 15:14:08 -0700

    eclipse-workspace

---
+++ ProjectPineapple/src/AwesomeJavaProgram.java
@@ -0,0 +1,32 @@
+
+public class AwesomeJavaProgram {
+
+	public static void main(String[] args) {
+		//int is type of variable (Integer)
+		//myInt is name of variable (can be whatever name)
+		//= 7 give myInt a value. Everytime you call myInt it will now its = to 7
+		int myInt = 7;
+		
+		//orange text such as double, char, and int are primative while System does not
+		//Note that primatives start with a lowercase
+		
+		//double is a decimal number 
+		double shoeSize = 9.5;
+		
+		//char is single character
+		char myInitial = 'V';
+		
+		//Stores result of myInt * shoeSize using double since it will come out as a decimal
+		double result = myInt * shoeSize;
+		
+		//String is similar to char but can hold up to more characters
+		String myName = "Victor";
+		//type out myName and push . to see a whole list of options you can use
+		
+		
+		//need to remember System.out.println();
+		System.out.println(myName.length());
+			
+	}
+
+}
